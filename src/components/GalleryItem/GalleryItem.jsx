b617ce775ddedbbcf9ac81679de9657469d9fc1b
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-03-26 16:52:01 -0500

    added toggle feature

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react'; 
+
 function GalleryItem( props ) {
+    const [showImage, setShowImage] = useState( false );
+
+    let displayImage = () => {
+        let display = <img src={props.path} />
+        if( showImage ) {
+            display = <p>{props.description}</p>
+        }
+        return display;
+    }
+
+    let toggleImage = () => {
+        setShowImage( !showImage );
+    }
     return(
         <>
-            <img src={props.path} />
+            <div onClick = {toggleImage}>{displayImage()}</div>    
         </>
     )
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -3,7 +3,7 @@ import GalleryItem from '../GalleryItem/GalleryItem';
 function GalleryList( props ) {
     return(
         <>
-            { props.gallery.map( image => <GalleryItem decription = { image.description } path = {image.path} id = {image.id} likes = { image.likes } /> )}
+            { props.gallery.map( image => <GalleryItem description = { image.description } path = {image.path} id = {image.id} likes = { image.likes } /> )}
         </>
     )
 }
