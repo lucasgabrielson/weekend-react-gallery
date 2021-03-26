e791c2bd8fd88fc84d5577f35e7b3f6527ca8b3a
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-03-26 16:10:49 -0500

    added git route and fran_cream_ale image

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -189,6 +189,7 @@ const galleryItems = [
 { id: 73, path: 'images/trim_castle_side.jpg', description: 'Trime Castle in Ireland.', likes: 0 },
 { id: 74, path: 'images/vet_services_pigeons.jpg', description: 'Vet services sign for your cow, dog, or pigeon.', likes: 0 },
 { id: 75, path: 'images/wheeler_lake.jpg', description: 'Snow fed mountain lake, Wheeler Lake, near Alma and Leadville, Colorado. The lake is at 12K ft elevation.', likes: 0 },
+{ id: 76, path: 'images/fran_cream_ale.jpg', description: 'Frances trying to drink a Castle Danger', likes: 0}
 
 ];
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios'; 
 
 function App() {
+    // load students on DOM load
+    useEffect(() => { getGallery() }, [])
+    
+    const [ gallery, setGallery ] = useState([]);
+
+    const getGallery = () => {
+      console.log( 'in getGalley' );
+      axios.get( '/gallery' )
+        .then( response => {
+          console.log( 'back from GET with', response.data);
+        })
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/fran_cream_ale.jpg" height='200' width='200'/>
       </div>
     );
 }
